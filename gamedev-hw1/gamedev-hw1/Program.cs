7ed8a007a041b2a83b0b3ac54c35517940307174
Omar <o.meneam@gmail.com>
2022-03-11 09:31:32 +0200

    gamedev-hw1

--- gamedev-hw1/gamedev-hw1/Program.cs
+++ gamedev-hw1/gamedev-hw1/Program.cs
@@ -6,9 +6,11 @@ namespace gamedev_hw1
     {
         static void Main(string[] args)
         {
+            // step 1 : variables
             int health = 100;
             string hero;
 
+            // step 2 : Damaged function
             int DamagedHealth(int damage)
             {
                 return health - damage;
@@ -22,14 +24,18 @@ namespace gamedev_hw1
                 return PlayerHp + ExtraHp;
             }
 
+            // Step 3 : Boost function
             int PlayerHp2 = boost(10);
             Console.WriteLine("Boosted Health : {0}", PlayerHp2);
 
+
+            // Step 4 : Health Array and Print the second digit of the array
             string[] HealthArray = { "80", "90" };
 
             int lastindex = HealthArray.Length -1;
             Console.WriteLine(HealthArray[lastindex]);
 
+            // Step 5 : Compare and print the result
             void Comparison()
             {
                 if (PlayerHp > PlayerHp2)
